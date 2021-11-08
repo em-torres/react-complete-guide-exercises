dd0904fa93f87e5a1f4415383389942bb1df556a
emarte_1973 <edwin.marte@newfold.com>
2021-11-08 17:32:23 -0400

    exercise 2

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import ExpenseForm from './ExpenseForm';
+import '../../css/NewExpense.css';
+
+const NewExpense = (props) => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+    };
+  };
+
+  return (
+    <div className='new-expense'>
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    </div>
+  );
+};
+
+export default NewExpense;
