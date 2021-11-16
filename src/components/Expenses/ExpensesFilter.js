dc48a6b9a7302ba7c4f3e4830ccdf0be3968ed30
emarte_1973 <edwin.marte@newfold.com>
2021-11-16 09:28:56 -0400

    Exercise 3

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -3,19 +3,15 @@ import React from 'react';
 import '../../css/ExpensesFilter.css';
 
 const ExpensesFilter = (props) => {
-  const onChangeHandler = (event) => {
-    const filteringData = {
-      year: event.target.value,
-    };
+  const dropdownChangeHandler = (event) => {
+    props.onChangeFilter(event.target.value);
+  };
 
-    props.onSaveExpenseFilterDataHandler(filteringData);
-  }
-  
   return (
     <div className='expenses-filter'>
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
-        <select value={props.selected} onChange={onChangeHandler}>
+        <select value={props.selected} onChange={dropdownChangeHandler}>
           <option value='2022'>2022</option>
           <option value='2021'>2021</option>
           <option value='2020'>2020</option>
